f9e100ef7bd854317a99b5cebdd802a8568dc10d
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 12:48:37 -0400

    import SingUp component to the app.js

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import SignUp from './Component/Users/SignUp'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     <h1>Hello World</h1>
+     <SignUp/>
       </div>
     );
   }
--- hacker-news/src/Component/Users/SignUp.js
+++ hacker-news/src/Component/Users/SignUp.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 
+const initialState = {
+  username: "",
+  password: ""
+};
+
 const SignUp = () => {
-  const [users, setUsers] = useState({ username: "", password: "" });
+  const [users, setUsers] = useState(initialState);
 
   const handleChange = e => {
-      setUsers({...users, [e.target.name]:e.target.value});
-  }
-  
+    setUsers({ ...users, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    event.preventDefault();
+    console.log(users);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <h1>Create a New Account</h1>
       <div>
         <input
           type="text"
@@ -17,6 +29,15 @@ const SignUp = () => {
           onChange={handleChange}
           value={users.username}
         />
+
+        <input
+          type="Email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={users.username}
+        />
+
         <input
           type="text"
           name="password"
