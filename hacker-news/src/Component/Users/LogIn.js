2bc8a283309c342bfa0808c7692c972ae304faa7
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 13:13:36 -0400

    added inputs and button to the LogIn component

--- hacker-news/src/Component/Users/LogIn.js
+++ hacker-news/src/Component/Users/LogIn.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {setUser} from 'react';
+
+const initialState = {
+    username: "",
+    password: ""
+  };
 
 const LogIn = () => {
+ const [creds, setcreds] = setUser( initialState) 
+
+ 
 
     return(
         <div>
-            
+            <input
+            type="text"
+            name="username"
+            placeholder="username"
+            onChange = {handleChange}
+            value={creds.username}/>
+            <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onchanged ={handleChange}
+            value={creds.password}/>
+            <button>LogIn</button>
         </div>
     )
 }
 
+export default LogIn;
