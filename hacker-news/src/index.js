084b0258353fe6d9a708eb4320d9eb8862e470bf
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 16:45:52 -0400

    Revert "Created Signup and LogIn component"

--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import "bulma/css/bulma.css";
-import { BrowserRouter} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  <App />,
+  document.getElementById('root')
 );
