1d42a2dd304d6d5ee59cc15504fbdbf8b99d782a
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 10:36:43 -0400

    added handleChange function to the SignUp component

--- hacker-news/src/Component/Users/SignUp.js
+++ hacker-news/src/Component/Users/SignUp.js
@@ -1,30 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const SignUp = () => {
-    const [users, setUsers] = useState({username:"", password:""})
-   
+  const [users, setUsers] = useState({ username: "", password: "" });
 
-    return(
-        <form>
-        <div>
-           <input
-           type="text"
-           name="username"
-           placeholder = "Username"
-           onChange={handleChange}
-           value={users.username}
-           />
-           <input
-           type="text"
-           name="password"
-           placeholder="password"
-           onChange={handleChange}
-           value={users.password}
-           />
-           <button>Sign Up</button>
-        </div>
-        </form>
-    )
-}
+  const handleChange = e => {
+      setUsers({...users, [e.target.name]:e.target.value});
+  }
+  
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+          value={users.username}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+          value={users.password}
+        />
+        <button>Sign Up</button>
+      </div>
+    </form>
+  );
+};
 
 export default SignUp;
