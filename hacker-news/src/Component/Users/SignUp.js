55d8f949b0390c3339ceb24aef4b9ea0097cb907
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 10:31:49 -0400

    added inputs and button tothe SignUp component

--- hacker-news/src/Component/Users/SignUp.js
+++ hacker-news/src/Component/Users/SignUp.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignUp = () => {
+    const [users, setUsers] = useState({username:"", password:""})
+   
 
     return(
+        <form>
         <div>
-
+           <input
+           type="text"
+           name="username"
+           placeholder = "Username"
+           onChange={handleChange}
+           value={users.username}
+           />
+           <input
+           type="text"
+           name="password"
+           placeholder="password"
+           onChange={handleChange}
+           value={users.password}
+           />
+           <button>Sign Up</button>
         </div>
+        </form>
     )
 }
 
