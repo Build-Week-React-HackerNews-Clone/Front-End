edfd3846c031e92314479f48b8525f79bfacfc0f
Manju KC <kcmanjuthapa@gmail.com>
2019-10-21 18:04:09 -0400

    added BrowserRouter on index.js

--- hacker-news/src/App.js
+++ hacker-news/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import LogIn from "./Component/Users/Login";
 import SignUp from "./Component/Users/SignUp";
 import "./App.css";
+import {BrowerRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
         <SignUp />
         <LogIn />
-      </div>
+      </Router>
     );
   }
 }
